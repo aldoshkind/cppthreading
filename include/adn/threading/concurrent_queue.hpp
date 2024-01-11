8b64f2f007ae0f9e0b57987e53537a4549954822
Dmitry <aldoshkind@yandex.ru>
2024-01-11 11:03:57 +0700

    Initial commit

---
+++ include/adn/threading/concurrent_queue.hpp
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <list>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <optional>
+
+namespace adn::threading
+{
+
+template <class type>
+class concurrent_queue
+{
+public:
+    typedef type value_type;
+    
+    concurrent_queue(std::optional<size_t> max_queue_length = {});
+	~concurrent_queue();
+
+	bool push(const type &value);
+	bool push_front(const type &value);
+	bool pop(type &value);
+    
+    void stop();
+    bool running() const {return !exited;}
+    
+    size_t size() const
+    {
+        std::unique_lock<decltype (mutex)> lock(mutex);
+        return elements.size();
+    }
+    
+private:
+    mutable std::recursive_mutex mutex;
+	std::condition_variable_any condvar;
+
+	std::list<type> elements;
+    
+    std::atomic<bool> exited = false;
+    
+    std::optional<size_t> max_length;
+};
+
+template <class type>
+concurrent_queue<type>::concurrent_queue(std::optional<size_t> max_queue_length) : max_length(max_queue_length)
+{
+	//
+}
+
+template <class type>
+concurrent_queue<type>::~concurrent_queue()
+{
+    stop();
+}
+
+template <class type>
+void concurrent_queue<type>::stop()
+{
+    exited = true;
+    std::unique_lock<decltype (mutex)> lock(mutex);
+    elements.clear();
+    condvar.notify_all();
+}
+
+template <class type>
+bool concurrent_queue<type>::push(const type &value)
+{
+    std::unique_lock<decltype (mutex)> lock(mutex);
+    if(exited)
+    {
+        return false;
+    }
+    if(max_length.has_value() and elements.size() >= max_length)
+    {
+        elements.pop_front();
+    }
+	elements.push_back(value);
+	condvar.notify_one();
+    
+    //printf("queue size is %ld\n", elements.size());
+
+	return true;
+}
+
+template <class type>
+bool concurrent_queue<type>::push_front(const type &value)
+{
+	std::unique_lock<decltype (mutex)> lock(mutex);
+    if(exited)
+    {
+        return false;
+    }
+    if(max_length.has_value() and elements.size() >= max_length)
+    {
+        elements.pop_front();
+    }
+	elements.push_front(value);
+	condvar.notify_one();
+
+	return true;
+}
+
+template <class type>
+bool concurrent_queue<type>::pop(type &value)
+{
+	std::unique_lock<decltype (mutex)> lock(mutex);
+    auto need_stop = [this](){return exited or !elements.empty();};
+    for( ; !need_stop() ; )
+    {
+        condvar.wait_for(lock, std::chrono::milliseconds(100), need_stop);
+    }
+    if(exited)
+    {
+        return false;
+    }
+    
+	value = elements.front();
+	elements.pop_front();
+
+	return true;
+}
+
+}
+
